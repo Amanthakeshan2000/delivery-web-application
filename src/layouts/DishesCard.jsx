1102a9cd63584491819463364bf3b893665d5a50
Amantha Keshan <amanthakeshangithub@gmail.com>
2024-07-20 20:02:38 +0530

    addd

--- src/layouts/DishesCard.jsx
+++ src/layouts/DishesCard.jsx
@@ -60,7 +60,7 @@ const DishesCard = (props) => {
             <h3 className="font-semibold text-lg text-gray-700">{props.price}</h3>
             <Button
               title="Add"
-              className="bg-blue-600 text-white hover:bg-blue-700 transition-colors duration-300"
+              className="bg-blue-600 text-black hover:bg-blue-700 transition-colors duration-300"
               onClick={handleAddClick}
             />
           </div>
