78b11b44de7579b303f0de1bef96f557271ed0fd
Amantha Keshan <amanthakeshangithub@gmail.com>
2024-07-22 14:43:26 +0530

    add

--- src/layouts/DishesCard.jsx
+++ src/layouts/DishesCard.jsx
@@ -39,7 +39,7 @@ const Popup = ({ dish, quantity, setQuantity, onClose, onAddToCart }) => {
     <div className="radio-content">
       <div className="radio-text">
         <span className="text-sm">Subscribe</span>
-        <span className="additional-info a">Subscribe and Save</span>
+        <span className="additional-info a">Subscribe Now</span>
       </div>
       <div className="flex items-center space-x-3">
         <span className="discount-banner-radio b">SAVE 21%</span>
