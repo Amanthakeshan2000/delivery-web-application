7294a629e05f0aa8ac3683115ea5e9137ce07c67
Amantha Keshan <amanthakeshangithub@gmail.com>
2024-07-23 09:29:30 +0530

    add

--- src/layouts/CartPopup.jsx
+++ src/layouts/CartPopup.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import "../css/CartPopup.css";
 
-
 const CartPopup = ({ cartItems, setCartItems, onClose }) => {
   useEffect(() => {
     // Add no-scroll class to body when the popup is open
@@ -29,15 +28,16 @@ const CartPopup = ({ cartItems, setCartItems, onClose }) => {
 
   const totalPrice = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2);
 
-  // Prevent default form submission behavior
-  const handleButtonClick = (event) => {
-    event.preventDefault();
-    // Add any other logic you need here
-  };
-
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-light-blue-100 border border-light-blue-300 p-6 rounded-lg shadow-lg w-full max-w-md mx-4 sm:mx-2 relative animate-slide-up mt-4 overflow-y-auto max-h-[90vh]">
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 text-white bg-red-600 hover:bg-red-700 rounded-full w-8 h-8 flex items-center justify-center transition-colors duration-300"
+          aria-label="Close"
+        >
+          &times;
+        </button>
         <h2 className="text-2xl font-semibold mb-4 text-center">Cart</h2>
         {cartItems.length > 0 ? (
           cartItems.map((item, index) => (
@@ -49,20 +49,20 @@ const CartPopup = ({ cartItems, setCartItems, onClose }) => {
               </div>
               <div className="flex items-center space-x-1">
                 <button
-                  onClick={(event) => { handleQuantityChange(index, -1); handleButtonClick(event); }}
+                  onClick={() => handleQuantityChange(index, -1)}
                   className="bg-green-500 text-white w-6 h-6 rounded-full flex items-center justify-center hover:bg-green-700 transition-colors duration-300"
                 >
                   -
                 </button>
                 <span className="text-xs">{item.quantity}</span>
                 <button
-                  onClick={(event) => { handleQuantityChange(index, 1); handleButtonClick(event); }}
+                  onClick={() => handleQuantityChange(index, 1)}
                   className="bg-green-500 text-white w-6 h-6 rounded-full flex items-center justify-center hover:bg-green-700 transition-colors duration-300"
                 >
                   +
                 </button>
                 <button
-                  onClick={(event) => { handleRemoveItem(index); handleButtonClick(event); }}
+                  onClick={() => handleRemoveItem(index)}
                   className="bg-red-500 text-white w-6 h-6 rounded-full flex items-center justify-center hover:bg-red-700 transition-colors duration-300"
                 >
                   &times;
