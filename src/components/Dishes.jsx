99ffd6c6a757c20ce1c2a5607be3c6dc9223159c
Amantha Keshan <amanthakeshangithub@gmail.com>
2024-07-31 10:55:00 +0530

    add

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -64,9 +64,10 @@ const Dishes = () => {
       <div className="flex flex-col gap-16 lg:px-10 px-5">
         {categories.map(category => (
           <div key={category.id} className="bg-white p-6 rounded-lg shadow-md mb-8 w-full lg:w-3/4 mx-auto">
-            <h2 className="text-5xl font-bold text-gray-800 mb-6 border-b-2 border-gray-200 pb-4 text-center">
-              {category.name}
-            </h2>
+          <h2 className="text-3xl sm:text-4xl font-bold text-gray-800 mb-6 border-b-2 border-gray-200 pb-4 text-center">
+  {category.name}
+</h2>
+
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
               {products[category.id] ? (
                 products[category.id].map(product => (
