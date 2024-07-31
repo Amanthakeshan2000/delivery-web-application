3e4bbd896ab87593e47c5e2a0af4d1e175ea1d97
Amantha Keshan <amanthakeshangithub@gmail.com>
2024-07-31 11:23:34 +0530

    change port

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 2000, // Specify the custom port here
     proxy: {
       '/api': {
         target: 'https://checkmateapi20240716235602.azurewebsites.net',
